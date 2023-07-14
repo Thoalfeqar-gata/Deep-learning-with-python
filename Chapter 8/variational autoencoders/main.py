515ad9e5b9fe427057d9a51bd0fe2cc73f586e5e
Thoalfeqar-gata <thoalfeqarkatajarullah@gmail.com>
2023-07-15 00:03:34 +0300

    Added face images generation using variational autoencoders

--- Chapter 8/variational autoencoders/main.py
+++ Chapter 8/variational autoencoders/main.py
@@ -5,13 +5,15 @@ from keras.utils import plot_model
 from keras.datasets import mnist
 from scipy.stats import norm
 from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
 import keras
-import numpy as np, tensorflow as tf
+import numpy as np, tensorflow as tf, os, cv2
 tf.compat.v1.disable_eager_execution()
 
-img_shape = (28, 28, 1)
+size = 160
+img_shape = (size, size, 3)
 batch_size = 16
-latent_dim = 2
+latent_dim = 10
 
 input_img = layers.Input(shape = img_shape)
 x = layers.Conv2D(32, 3, padding = 'same', activation = 'relu')(input_img)
@@ -38,7 +40,7 @@ decoder_input = layers.Input(K.int_shape(z)[1:])
 x = layers.Dense(np.prod(shape_before_flattening[1:]), 'relu')(decoder_input)
 x = layers.Reshape(shape_before_flattening[1:])(x)
 x = layers.Conv2DTranspose(32, 3, padding = 'same', activation = 'relu', strides = (2, 2))(x)
-x = layers.Conv2D(1, 3, padding = 'same', activation = 'sigmoid')(x)
+x = layers.Conv2D(3, 3, padding = 'same', activation = 'sigmoid')(x)
 
 decoder = Model(decoder_input, x)
 z_decoded = decoder(z)
@@ -56,7 +58,7 @@ class CustomVariationalLayer(keras.layers.Layer):
     def call(self, inputs):
         x = inputs[0]
         z_decoded = inputs[1]
-        loss =self.vae_loss(x, z_decoded)
+        loss = self.vae_loss(x, z_decoded)
         self.add_loss(loss, inputs = inputs)
         return x
 
@@ -67,28 +69,38 @@ vae.compile(optimizer = 'rmsprop', loss = None)
 vae.summary()
 plot_model(vae, to_file = 'Chapter 8/variational autoencoders/model/model.png', expand_nested = True, dpi = 128)
 
-(x_train, _), (x_test, _) = mnist.load_data()
+images = []
+for dir, dirnames, filenames in os.walk('Chapter 8/variational autoencoders/Data/lfw_funneled'): #the data is not included because of its large size
+    if len(filenames) <= 0:
+        continue
+    
+    for filename in filenames:
+        image = cv2.resize(cv2.imread(os.path.join(dir, filename)), (size, size))
+        images.append(image)
+    
+total_images = len(images)
+test_size = 1000
+train_size = total_images - test_size
+x_train = np.asarray(images[:train_size]) / 255.
+x_test = np.asarray(images[train_size:]) / 255.
 
-x_train = x_train.astype(np.float32) / 255.
-x_train = np.expand_dims(x_train, axis = -1)
-x_test = x_test.astype(np.float32) / 255.
-x_test = np.expand_dims(x_test, axis = -1)
+vae.fit(x = x_train, y = None, shuffle = True, epochs = 10, batch_size = batch_size, validation_data = (x_test, None))
 
-vae.fit(x = x_train, y = None, shuffle = True, epochs = 20, batch_size = batch_size, validation_data = (x_test, None))
-
-n = 15
-digit_size = 28
-figure = np.zeros((digit_size * n, digit_size * n))
+n = 7
+image_size = size
+figure = np.zeros((image_size * n, image_size * n, 3))
 grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
 grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
 
 for i, yi in enumerate(grid_y):
     for j, xi in enumerate(grid_x):
-        z_sample = np.array([[xi, yi]])
+        z_sample = np.array([[xi, yi, xi, yi, xi, yi, xi, yi, xi, yi]])
         x_decoded = decoder.predict(z_sample)
-        digit = x_decoded[0].reshape(digit_size, digit_size)
-        figure[i * digit_size : (i + 1) * digit_size, j * digit_size : (j + 1) * digit_size] = digit
+        image = x_decoded[0].reshape(image_size, image_size, 3)
+        figure[i * image_size : (i + 1) * image_size, j * image_size : (j + 1) * image_size, :] = image
         
-plt.figure(figsize = (10, 10))
-plt.imshow(figure, cmap = 'Greys_r')
+cv2.imshow('hi', figure)
+cv2.waitKey(0)
+plt.figure(figsize = (15, 15))
+plt.imshow(figure)
 plt.show()
